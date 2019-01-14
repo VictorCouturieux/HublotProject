552b8fc3e265093a4e49631514907fc09dea9b2e
VictorCouturieux <victor.couturieux@sas-impact.fr>
2019-01-14 13:51:55 +0100

    init Project

---
+++ HublotProject/Assets/Resources/fishAnim/scrip/animFish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class animFish : MonoBehaviour
+{
+    public GameObject obj;
+    public int speed = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 vct = new Vector3(obj.transform.position.x ,obj.transform.position.y, obj.transform.position.z);
+        
+        transform.RotateAround(vct, Vector3.up, speed * Time.deltaTime );
+//        transform.Rotate();
+    }
+}
---
+++ HublotProject/Assets/Resources/scriptCamera/MoveCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    
+    public GameObject player;
+    private Vector3 offset;
+    private Quaternion offQ;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        offQ = new Quaternion(
+            transform.rotation.x - player.transform.rotation.x,
+            transform.rotation.y - player.transform.rotation.y,
+            transform.rotation.z - player.transform.rotation.z,
+            transform.rotation.w - player.transform.rotation.w
+            ); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newXPosition = player.transform.position.x - offset.x;
+        float newZPosition = player.transform.position.z - offset.z;
+ 
+        transform.position = new Vector3(
+            player.transform.position.x, 
+            player.transform.position.y, 
+            player.transform.position.z-0.2f);
+
+        transform.rotation = new Quaternion(
+            player.transform.rotation.x,
+            player.transform.rotation.y ,
+            transform.rotation.z,
+            player.transform.rotation.w);
+    }
+}
